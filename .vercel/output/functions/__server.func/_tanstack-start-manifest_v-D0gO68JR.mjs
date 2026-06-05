1dc387e45b2db2eb889188c5edf038e2acf620d0
akramuddin53-lang <akramuddin53@gmail.com>
2026-06-05 12:11:22 +0600

    Updated restaurant.html

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-D0gO68JR.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/tmp/cc-agent/67554192/project/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-CDMVERaz.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-CDMVERaz.js" } }] }, "/": { filePath: "/tmp/cc-agent/67554192/project/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-D4ODa_Rl.js"] } } });
-export {
-  tsrStartManifest
-};
